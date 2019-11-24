90f7208f105814f027bdc6223b07756227ff7061
dpetrovs <dpetrovs@evolutiongaming.com>
2019-11-24 18:44:42 +0200

    Implement pgn list and tag parsing

--- pypgn/game.py
+++ pypgn/game.py
@@ -1,17 +1,31 @@
-def _get_pgn_string(path: str) -> str:
+import re
+
+from typing import Mapping
+
+
+def _get_pgn_list(path: str) -> list:
     with open(file=path, mode='r') as f:
-        pgn: str = f.read()
-        return pgn
+        lines = f.read().splitlines()
+        return lines
+
+
+def _get_tags(pgn: list) -> Mapping:
+    tag_list = [tag for tag in pgn if re.search(r'^\[', tag)]
+    tag_dict: dict = {}
+    for tag in tag_list:
+        key = tag.split(" ", 1)[0][1:]
+        val = tag.split(" ", 1)[1][1:-2]
+        tag_dict[key] = val
+
+    return tag_dict
 
 
 class Game:
     def __init__(self, file_path):
-        self.pgn = _get_pgn_string(file_path)
+        self.pgn: list = _get_pgn_list(file_path)
+        self.tags: Mapping = _get_tags(self.pgn)
 
     def get_tag(self, name: str):
-        pass
-
-    def get_tags(self):
         pass
 
     def get_movetext(self):
@@ -22,3 +36,7 @@ class Game:
 
     def get_move_count(self):
         pass
+
+
+game = Game('test.pgn')
+game.get_tags()
