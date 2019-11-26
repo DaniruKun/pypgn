4d3416a207abf70b439312f7b25d912685481dfa
dpetrovs <dpetrovs@evolutiongaming.com>
2019-11-26 22:45:21 +0200

    Bump version to 0.3.0

--- setup.py
+++ setup.py
@@ -1,6 +1,5 @@
 from setuptools import setup
 
-# read the contents of your README file
 from os import path
 this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
@@ -10,7 +9,7 @@ setup(
     name='pypgn',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    version='0.2.1',
+    version='0.3.0',
     packages=['pypgn'],
     url='https://github.com/DaniruKun/pypgn',
     license='MPL-2.0',
