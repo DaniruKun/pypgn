161d8bef371fe164282cea22ac853f9a84e7b690
Daniils Petrovs <5202322+DaniruKun@users.noreply.github.com>
2019-11-30 15:21:32 +0200

    Changes to public API (#10)
    * Add argument safety to several public methods
    
    * Implement test for non existing tag in PGN file tags
    
    * Remove redundant test and add safety to http conns
    
    * Rename public methods to be shorter
    
    * Bump version to minor and update README
    
    * Add event getter, improve missing tag exception test
    
    * Remove duplicate dependency pytest

--- test/test_game.py
+++ test/test_game.py
@@ -14,52 +14,47 @@ def game_pgn():
 
 class TestGame:
     def test_get_pgn_list(self, game_pgn):
-        assert len(game_pgn.get_pgn_list()) == 20
-
-    def test_get_tags(self, game_pgn):
-        assert type(game_pgn.get_tags()) == dict
+        assert len(game_pgn.pgn_list()) == 20
 
     def test_get_tag(self, game_pgn):
-        assert game_pgn.get_tag('Event') == "Rated Blitz game"
-        assert game_pgn.get_tag('Site') == "https://lichess.org/#"
-        assert game_pgn.get_tag('UTCDate') == "2019.11.06"
+        assert game_pgn.tag('Event') == "Rated Blitz game"
+        assert game_pgn.tag('Site') == "https://lichess.org/#"
+        assert game_pgn.tag('UTCDate') == "2019.11.06"
 
     def test_get_non_existing_tag(self, game_pgn):
-        with pytest.raises(KeyError):
-            game_pgn.get_tag('SomeTag')
+        tag = 'SomeTag'
+        with pytest.raises(KeyError, match=fr'{tag}'):
+            game_pgn.tag(tag)
 
     def test_get_move(self, game_pgn):
-        assert game_pgn.get_move(4) == ["4.", "d4", "Nc6"]
+        assert game_pgn.move(4) == ["4.", "d4", "Nc6"]
 
     def test_get_moves(self, game_pgn):
-        moves = game_pgn.get_moves()
+        moves = game_pgn.moves
         assert type(moves) == list
         assert moves[0] == ["1.", "e4", "c6"]
 
     def test_get_ply(self, game_pgn):
-        for i in range(1, game_pgn.get_move_count()):
-            move = game_pgn.get_move(i)
-            assert move[1] == game_pgn.get_ply(i, 'w') and move[2] == game_pgn.get_ply(i, 'b'), \
+        for i in range(1, game_pgn.move_count()):
+            move = game_pgn.move(i)
+            assert move[1] == game_pgn.ply(i, 'w') and move[2] == game_pgn.ply(i, 'b'), \
                 "Non-matching ply pair at move %s !" % str(i)
 
-    def test_get_move_count(self, game_pgn):
-        assert game_pgn.get_move_count() == len(game_pgn.get_moves())
-
     def test_get_result(self, game_pgn):
-        assert game_pgn.get_result() == game_pgn.get_tag('Result')
+        assert game_pgn.result() == game_pgn.tag('Result')
 
     def test_get_date(self, game_pgn):
-        assert game_pgn.get_date() == game_pgn.get_tag('UTCDate')
+        assert game_pgn.date() == game_pgn.tag('UTCDate')
 
     def test_get_move_range(self, game_pgn):
-        assert game_pgn.get_move_range(1, 28) == game_pgn.get_moves()
+        assert game_pgn.move_range(1, 28) == game_pgn.moves
 
     def test_init_from_lichess_id(self):
         game_lichess = Game('dGm3ND39')
         time.sleep(1)
-        assert game_lichess.get_move_count() == 18
+        assert game_lichess.move_count() == 18
 
     def test_init_from_lichess_url(self):
         game_lichess = Game('https://lichess.org/dGm3ND39')
         time.sleep(1)
-        assert game_lichess.get_move_count() == 18
+        assert game_lichess.move_count() == 18
